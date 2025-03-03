6521cdb768166b9d87944f69ab3eaad7ccdee75a
hariachmad <hari08achmad@gmail.com>
2025-03-03 15:57:50 +0700

    ADD WELLCOME

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,11 +2,13 @@ import { useContext } from "react";
 import { FormCard } from "../components/formCard";
 
 export const Main = () => {
+  const {user} = useContext(AuthContext);
   return (
     <>
       <div className="bg-gradient-to-r from-green-950 to-green-200 h-screen w-screen">
         <section className="header rounded-b-md h-[15vh] bg-white shadow-2xl flex justify-between items-center px-5 ">
           <img className="w-60 h-15" src="/logo-perhutani.png"></img>
+          <h2 className="absolute right-8 top-15">Selmat Datang, {user}</h2>
         </section>
         <section className="content flex justify-center mt-[20px]">
           <FormCard></FormCard>
