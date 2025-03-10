5f57ef9bbfb50ea0444e684b3583acc52033d498
hariachmad <hari08achmad@gmail.com>
2025-03-10 14:58:24 +0700

    TYPO REVISION

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -9,7 +9,7 @@ export const Main = () => {
       <div className="bg-gradient-to-r from-green-950 to-green-200 h-screen w-screen">
         <section className="header rounded-b-md h-[15vh] bg-white shadow-2xl flex justify-between items-center px-5 ">
           <img className="w-60 h-15" src="/logo-perhutani.png"></img>
-          <h2 className="absolute right-8 top-15">Selmat Datang, {user}</h2>
+          <h2 className="absolute right-8 top-15">Selamat Datang, {user}</h2>
         </section>
         <section className="content flex justify-center mt-[20px]">
           <FormCard></FormCard>
